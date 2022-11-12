900d91e54f1fab5ca3078473d6d41199ebd3c6bf
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2022-11-12 16:44:43 -0500

    Magic 8 Ball App in Swift

--- Magic-8-Ball/ViewController.swift
+++ Magic-8-Ball/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var magicBallImageView: UIImageView!
+
+    @IBOutlet weak var rightHandImageView: UIImageView!
+    @IBOutlet weak var leftHandImageView: UIImageView!
+    //    In order to use the images of our assets in the Array we use: #imageLiteral()
+    let arrayOfMagicBalls = [ #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball5"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball1")];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        leftHandImageView.isHidden = true;
+        rightHandImageView.isHidden = true;
     }
 
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        magicBallImageView.image = arrayOfMagicBalls.randomElement();
+        
+        leftHandImageView.isHidden = false;
+        rightHandImageView.isHidden = false;
+    }
+    
 }
 
